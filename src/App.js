38fb4d31fb7aefa271097ac8efbb8bc04c746c39
blue0316 <greatblueknight@gmail.com>
2023-11-22 14:49:53 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AnimatedGradientBackground from "./components/AnimatedGradientBackground";
 
 function App() {
+  const [active, setActive] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        <AnimatedGradientBackground>
+          <div className="flex justify-center items-center h-screen">
+            <div className="flex relative flex-row items-center w-[420px] bg-white border border-solid border-[#fff] hover:border-[#ddd] focus:border-[#000] rounded-[26px] text-[#333] outline-0">
+              <input
+                type="password"
+                name="password"
+                autoFocus="true"
+                placeholder="Enter password..."
+                className="rounded-[26px] border-none text-[#333] text-[18px] leading-none outline-0 px-[25px] py-[15px] w-full"
+                onChange={(e) => {
+                  if (e.target.value !== "") setActive(true);
+                  else setActive(false);
+                }}
+              />
+              <button
+                type="button"
+                className={
+                  active
+                    ? "w-fit h-10 mr-[7px] text-[15px] text-white font-bold bg-black px-[20px] pb-px rounded-full transition-all duration-300"
+                    : "w-fit h-10 mr-[7px] text-[15px] text-white font-bold bg-[#222] bg-opacity-20 px-[20px] pb-px rounded-full cursor-not-allowed transition-all duration-300"
+                }
+                onClick={(e) => {
+                  console.log("clicked");
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </AnimatedGradientBackground>
+      </main>
+      <footer></footer>
     </div>
   );
 }
