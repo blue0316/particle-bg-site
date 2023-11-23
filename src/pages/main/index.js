1da95ad12919360dc22cc492e118fa56e2268f4a
blue0316 <greatblueknight@gmail.com>
2023-11-22 19:20:16 -0600

    update password input border color

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import AnimatedGradientBackground from "../../components/AnimatedGradientBackground";
 import { useNavigate } from "react-router-dom";
+import * as cn from "classnames";
 
 function MainPage() {
   const [password, setPassword] = useState("");
   const [active, setActive] = useState(false);
+  const [error, setError] = useState(false);
 
   const navigation = useNavigate();
 
@@ -14,18 +16,24 @@ function MainPage() {
     } else if (password === "two") {
       navigation("/two");
     } else {
-      window.alert("Your password is incorrect.");
+      setError(true);
     }
   };
 
   return (
     <AnimatedGradientBackground>
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex relative flex-row items-center w-[420px] bg-white border border-solid border-[#fff] hover:border-[#ddd] focus:border-[#000] rounded-[26px] text-[#333] outline-0">
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div
+          className={cn({
+            "flex relative flex-row items-center w-[420px] bg-white border border-solid hover:border-[#ddd] focus:border-[#000] rounded-[26px] text-[#333] outline-0": true,
+            "!border-red-500": error,
+            "border-white": !error,
+          })}
+        >
           <input
             type="password"
             name="password"
-            autoFocus="true"
+            autoFocus={true}
             placeholder="Enter password..."
             className="rounded-[26px] border-none text-[#333] text-[18px] leading-none outline-0 px-[25px] py-[15px] w-screen"
             onChange={(e) => {
@@ -41,16 +49,25 @@ function MainPage() {
           />
           <button
             type="button"
-            className={
-              active
-                ? "w-fit h-10 mr-[7px] text-[15px] text-white font-bold bg-black px-[20px] pb-px rounded-full transition-all duration-300"
-                : "w-fit h-10 mr-[7px] text-[15px] text-white font-bold bg-[#222] bg-opacity-20 px-[20px] pb-px rounded-full cursor-not-allowed transition-all duration-300"
-            }
+            className={cn({
+              "w-fit h-10 mr-[7px] text-[15px] text-white font-bold px-[20px] pb-px rounded-full transition-all duration-300": true,
+              "bg-black": active,
+              "bg-[#222] bg-opacity-20 cursor-not-allowed": !active,
+            })}
             onClick={clickHandler}
           >
             Submit
           </button>
         </div>
+        <p
+          className={cn({
+            "text-red-500 w-[420px] text-left px-4 mt-1 font-normal": true,
+            "opacity-100 visible": error,
+            "opacity-0 invisible": !error,
+          })}
+        >
+          Password is incorrect.
+        </p>
       </div>
     </AnimatedGradientBackground>
   );
